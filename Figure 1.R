7a92bb8aa55f09c51a18ffca528ada86336322ef
mccannecology <mccannmikejames@gmail.com>
2014-02-25 08:22:54 -0500

    More labels. hypothetical plots with log scale x-axis

--- Figure 1.R
+++ Figure 1.R
@@ -19,7 +19,8 @@ dataFPsmall$FPcover_max_percent <- dataFPsmall$FPcover_max*100
 # LINEAR       #
 ################ 
 a1 <- ggplot(data.frame(x = c(0, 0.5)), aes(x)) # set-up blank plot 
-a1 <- a1 + geom_segment(aes(x=0,y=1,xend=0.5,yend=100),size=1) # add line
+a1 <- a1 + scale_x_log10() # x-axis on log scale
+a1 <- a1 + geom_segment(aes(x=0,y=1,xend=0.5,yend=100),size=1) # add linear line
 a1 <- a1 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)") # label axes 
 a1 <- a1 + geom_text(aes(x=0.01,y=100,label="a)"),size=7) # add pane label
 a1
@@ -30,11 +31,12 @@ a1
 # SEGMENTED    # 
 # THRESHOLD    #
 ################
-a2 <- ggplot(data.frame(x = c(0, 0.5)), aes(x)) 
-a2 <- a2 + geom_segment(aes(x=0,y=1,xend=0.25,yend=10),size=1) + geom_segment(aes(x=0.25,y=90,xend=0.5,yend=100),size=1)
-a2 <- a2 + geom_vline(xintercept=0.25,colour="red",size=1)
-a2 <- a2 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)")
-a2 <- a2 + geom_text(aes(x=0.01,y=100,label="b)"),size=7)
+a2 <- ggplot(data.frame(x = c(0, 0.5)), aes(x)) # set-up blank plot 
+a2 <- a2 + scale_x_log10() # x-axis on log scale
+a2 <- a2 + geom_segment(aes(x=0,y=1,xend=0.25,yend=10),size=1) + geom_segment(aes(x=0.25,y=90,xend=0.5,yend=100),size=1) # add two segments 
+a2 <- a2 + geom_vline(xintercept=0.25,colour="red",size=1) # add vertical line @ threshold value 
+a2 <- a2 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)") # label axes 
+a2 <- a2 + geom_text(aes(x=0.01,y=100,label="b)"),size=7) # add pane label 
 a2
 
 ################
@@ -44,12 +46,13 @@ a2
 # ALT. STATES  #
 ################
 # create a blank plot - but you cannot view it until you add a line 
-a3 <- ggplot(data.frame(x = c(0, 0.5)), aes(x)) 
-a3 <- a3 + geom_segment(aes(x=0,y=1,xend=0.3,yend=10),size=1) + geom_segment(aes(x=0.2,y=90,xend=0.5,yend=100),size=1)
-a3 <- a3 + geom_vline(xintercept=0.20,colour="red",size=1)
-a3 <- a3 + geom_vline(xintercept=0.30,colour="red",size=1)
-a3 <- a3 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)") 
-a3 <- a3 + geom_text(aes(x=0.01,y=100,label="c)"),size=7) 
+a3 <- ggplot(data.frame(x = c(0, 0.5)), aes(x)) # set-up blank plot 
+a3 <- a3 + scale_x_log10() # x-axis on log scale
+a3 <- a3 + geom_segment(aes(x=0,y=1,xend=0.3,yend=10),size=1) + geom_segment(aes(x=0.2,y=90,xend=0.5,yend=100),size=1) # add two segments 
+a3 <- a3 + geom_vline(xintercept=0.20,colour="red",size=1) # add vertical line @ 1st threshold value 
+a3 <- a3 + geom_vline(xintercept=0.30,colour="red",size=1) # add vertical line @ 2nd threshold value 
+a3 <- a3 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)") # label axes 
+a3 <- a3 + geom_text(aes(x=0.01,y=100,label="c)"),size=7) # add pane label 
 a3
 
 ################
@@ -58,12 +61,13 @@ a3
 # "LINEAR"     #
 # (LOGISTIC)   #
 ################ 
+b1 <- ggplot(data=dataFPsmall, aes(x=TOTP_avg,y=as.factor(FPcover_max_percent))) + geom_point(shape=1) +geom_smooth(method="glm",family=binomial)
 b1 <- ggplot(data=dataFPsmall, aes(x=TOTP_avg,y=FPcover_max_percent)) + geom_point(shape=1) 
 b1 <- b1 + scale_x_log10()
 b1 <- b1 + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)")
 b1 <- b1 + geom_text(aes(x=0.01,y=100,label="d)"),size=7)
 
-b1 <- b1 + geom_smooth(method="glm", family=binomial) # add logistic regression here - problem: y(0,100) not (0,1)
+b1 <- b1 + geom_smooth(method="glm", family=quasibinomial) # add logistic regression here - problem: y(0,100) not (0,1) # may need to covert to "factor
 
 b1  
 
