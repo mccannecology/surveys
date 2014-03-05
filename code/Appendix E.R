2edba4e80009cae2d5961be1172459b56538dc8a
mccannecology <mccannmikejames@gmail.com>
2014-03-05 10:33:00 -0500

    re-done, including two outliers

--- code/Appendix E.R
+++ code/Appendix E.R
@@ -5,6 +5,30 @@
 # Total P and floating plant cover                                   # 
 ######################################################################
 
+library(ggplot2)
+
+# dataFPsmall - no outliers removed 
+appendixE <- ggplot(dataFPsmall,aes(x=TOTP_avg,y=FPcover_max)) + geom_point() 
+
+appendixE <- appendixE + stat_smooth(method="glm", family=binomial,col="red", se=F) 
+
+appendixE <- appendixE + stat_smooth(method="lm",col="blue", se=F)
+
+appendixE <- appendixE + xlab("Total phosphorus (mg/L)") + ylab("Floating plant cover(%)")
+
+appendixE <- appendixE + ylim(0,1) 
+
+y_breaks <- seq(0,1,0.25)
+
+y_labels <- as.character(y_breaks*100)
+
+appendixE <- appendixE + scale_y_continuous(breaks=y_breaks,labels=y_labels)
+
+appendixE <- appendixE + scale_x_log10() 
+
+appendixE
+
+
 # without Total P values >0.5 mg/L 
 appendixE <- ggplot(dataFPoutlierssmall,aes(x=TOTP_avg,y=FPcover_max)) + geom_point() 
 
