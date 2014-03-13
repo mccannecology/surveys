b10b823b63ee2499aaffe68e96b7c7fcc43b095b
mccannecology <mccannmikejames@gmail.com>
2014-03-13 13:34:38 -0400

    working on HJL comments

--- code/HJLcomments.R
+++ code/HJLcomments.R
@@ -7,7 +7,13 @@ library(ggplot2)
 ###########################################################################################
 # include 8 waterbodies that shift as separate points in Fig. 1
 ###########################################################################################
+dataSHIFTStwice = read.csv("survey analysis shiftsoccurtwice.csv") # import data matrix 
 
+# plot the data 
+j <- ggplot(data=dataSHIFTStwice, aes(x=TOTP_avg,y=FPcover_max)) + geom_point(shape=1) 
+j <- j + xlab("Total P (mg/L)") + ylab("Proportion FP cover")
+j <- j + scale_x_log10() 
+j
 
 ###########################################################################################
 # Fit mixed distribution to frequency data - Fig. 2 - compare two- vs. one-distribution
@@ -30,12 +36,10 @@ summary(glmFPTOTPbinomial)
 # segmented logistic regression
 breaks <- dataONEperpond$TOTP_avg[which(dataONEperpond$TOTP_avg >= 0.00001 & dataONEperpond$TOTP_avg <= 0.2)]    # create a vector to hold potential breakpoints 
 mse <- numeric(length(breaks)) # create a blank vector to hold MSE     
-
 for(i in 1:length(breaks)){ # loop over all of the potential breakpoints & actually try them out in a lm()
   piecewise <- glm(FPcover_max ~ TOTP_avg*(TOTP_avg < breaks[i]) + TOTP_avg*(TOTP_avg>=breaks[i]), family = binomial, data=dataONEperpond)
   mse[i] <- summary(piecewise)[4] # If this is a lm() I should index [6], if it's a glm() I should index [4]
 }
-
 mse <- as.numeric(mse) # converts list to numeric 
 breakpoint05<-breaks[which(mse==min(mse))] # picks the breakpoint with the lowest mse
 breakpoint05
@@ -63,25 +67,31 @@ b2allponds <- b2allponds + theme(axis.title.y=element_blank())
 b2allponds
 
 # partitioned logistic regression 
-
 # Add new variables to the data frame just for plotting the partitioned data in ggplot2
 # partition data: FPcover_max >= 0.6666 and FPcover_max < 0.6666 
 dataONEperpond$third <- ifelse(dataONEperpond$FPcover_max >= 0.6666, "greater", "lesser")
 
 # partitioned 33:67 y-variable 
-glmupperthird <- glm(FPcover_max ~ TOTP_avg, family = binomial, data = subset(dataONEperpond, dataONEperpond >= 0.6666)) 
-glmlowerthird <- glm(FPcover_max ~ TOTP_avg, family = binomial, data = subset(dataONEperpond, dataONEperpond < 0.6666)) 
+glmupperthird <- glm(FPcover_max ~ TOTP_avg, family = binomial, data = subset(dataONEperpond, dataONEperpond$FPcover_max >= 0.6666)) 
+glmlowerthird <- glm(FPcover_max ~ TOTP_avg, family = binomial, data = subset(dataONEperpond, dataONEperpond$FPcover_max < 0.6666)) 
 summary(glmupperthird)
 summary(glmlowerthird)
-ggplot(dataONEperpond,aes(x=TOTP_avg,y=FPcover_max)) + geom_point() + stat_smooth(method=glm, family=binomial, se=F,aes(fill=factor(third))) + scale_x_log10()
 
-######
-########
-#Error in family$linkfun(mustart) : 
-#  Argument mu must be a nonempty numeric vector
-#In addition: There were 13 warnings (use warnings() to see them)
-########
-######
+# plot it
+b3allponds <- ggplot(dataONEperpond,aes(x=TOTP_avg,y=FPcover_max)) + geom_point() 
+b3allponds <- b3allponds + stat_smooth(method=glm, family=binomial, se=F,aes(fill=factor(third))) 
+b3allponds <- b3allponds + xlab("Total P (mg/L)") + ylab("Floating plant cover(%)")
+b3allponds <- b3allponds + scale_x_log10()
+b3allponds <- b3allponds + theme(legend.position="none")
+y_breaks <- seq(0,1,0.25)
+y_labels <- as.character(y_breaks*100)
+b3allponds <- b3allponds + scale_y_continuous(breaks=y_breaks,labels=y_labels)
+b3allponds <- b3allponds + geom_vline(xintercept=0.01972,colour="red",size=1,linetype="longdash") # add vertical line @ lower threshold value 
+b3allponds <- b3allponds + geom_vline(xintercept=0.2151,colour="red",size=1,linetype="longdash") # add vertical line @ upper threshold value 
+b3allponds <- b3allponds + theme_classic(base_size=18) + theme(legend.position="none")
+b3allponds <- b3allponds + theme(axis.title.y=element_blank())
+b3allponds 
+
 
 ###########################################################################################
 # Include ponds w/o FP in analyses - Fig. 2
