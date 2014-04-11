c4ef577d16e88d801bc256a74c7576c31e1fbbf2
mccannecology <mccannmikejames@gmail.com>
2014-04-11 08:16:00 -0400

    added all of the data frames that will get used by gamlss and betareg with quadratic dispersion (no NAs)

--- code/beta regression gamlss.R
+++ code/beta regression gamlss.R
@@ -9,9 +9,7 @@ library(gamlss)
 
 # Get rid of any 0s and 1s in the dependent (Y) variable
 # Transformation suggested by: Smithson M, Verkuilen J (2006). Psychological Methods, 11(1), 54–71
-#
 # IMPORTANT: Re-import these data sets when you are done with beta regression analysis 
-#
 dataONEperpond$FPcover_max[dataONEperpond$FPcover_max == 1] <- (1*(length(dataONEperpond$FPcover_max)-1)+0.5)/(length(dataONEperpond$FPcover_max))
 dataONEperpond$FPcover_max[dataONEperpond$FPcover_max == 0] <- (0*(length(dataONEperpond$FPcover_max)-1)+0.5)/(length(dataONEperpond$FPcover_max))
 
@@ -21,15 +19,20 @@ dataFP$FPcover_max[dataFP$FPcover_max == 0] <- (0*(length(dataFP$FPcover_max)-1)
 dataFPsmall$FPcover_max[dataFPsmall$FPcover_max == 1] <- (1*(length(dataFPsmall$FPcover_max)-1)+0.5)/(length(dataFPsmall$FPcover_max))
 dataFPsmall$FPcover_max[dataFPsmall$FPcover_max == 0] <- (0*(length(dataFPsmall$FPcover_max)-1)+0.5)/(length(dataFPsmall$FPcover_max))
 
+dataONEperpondoutliers$FPcover_max[dataONEperpondoutliers$FPcover_max == 1] <- (1*(length(dataONEperpondoutliers$FPcover_max)-1)+0.5)/(length(dataONEperpondoutliers$FPcover_max))
+dataONEperpondoutliers$FPcover_max[dataONEperpondoutliers$FPcover_max == 0] <- (0*(length(dataONEperpondoutliers$FPcover_max)-1)+0.5)/(length(dataONEperpondoutliers$FPcover_max))
+
 dataFPoutliers$FPcover_max[dataFPoutliers$FPcover_max == 1] <- (1*(length(dataFPoutliers$FPcover_max)-1)+0.5)/(length(dataFPoutliers$FPcover_max))
 dataFPoutliers$FPcover_max[dataFPoutliers$FPcover_max == 0] <- (0*(length(dataFPoutliers$FPcover_max)-1)+0.5)/(length(dataFPoutliers$FPcover_max))
 
 dataFPoutlierssmall$FPcover_max[dataFPoutlierssmall$FPcover_max == 1] <- (1*(length(dataFPoutlierssmall$FPcover_max)-1)+0.5)/(length(dataFPoutlierssmall$FPcover_max))
 dataFPoutlierssmall$FPcover_max[dataFPoutlierssmall$FPcover_max == 0] <- (0*(length(dataFPoutlierssmall$FPcover_max)-1)+0.5)/(length(dataFPoutlierssmall$FPcover_max))
 
+
 # Plot the different data sets
 # Use these later for adding fitted models 
 plot(dataONEperpond$FPcover_max ~ dataONEperpond$TOTP_avg,main="dataONEperpond",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+plot(dataONEperpondoutliers$FPcover_max ~ dataONEperpondoutliers$TOTP_avg,main="dataONEperpondoutliers",xlab="Total P (mg/L)",ylab="FP cover",log="x")
 plot(dataFP$FPcover_max ~ dataFP$TOTP_avg,main="dataFP",xlab="Total P (mg/L)",ylab="FP cover",log="x")
 plot(dataFPsmall$FPcover_max ~ dataFPsmall$TOTP_avg,main="dataFPsmall",xlab="Total P (mg/L)",ylab="FP cover",log="x")
 plot(dataFPoutliers$FPcover_max ~ dataFPoutliers$TOTP_avg,main="dataFPoutliers",xlab="Total P (mg/L)",ylab="FP cover",log="x")
@@ -53,6 +56,53 @@ dataONEperpond_gamlss
 
 gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataONEperpond_flexmix, family=BE)
 
+####################### 
+# Beta regression     #
+# dataONEperpond      #
+# link: logit         #
+# Variable dispersion #
+# quadratic dispersion#
+#######################
+head(dataONEperpond)
+
+# re-format data so it has no NAs 
+dataONEperpond_gamlss <- cbind(dataONEperpond$FPcover_max,dataONEperpond$TOTP_avg)
+dataONEperpond_gamlss <- as.data.frame(dataONEperpond_gamlss) # convert to a data frame 
+dataONEperpond_gamlss <- dataONEperpond_gamlss[complete.cases(dataONEperpond_gamlss),]# remove any NAs
+names(dataONEperpond_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataONEperpond_gamlss)
+dataONEperpond_gamlss
+
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE)
+# Error in RS() : The global deviance is increasing 
+# Try different steps for the parameters or the model maybe inappropriate
+# In addition: 
+# Warning message: In glim.fit(f = sigma.object, X = sigma.X, y = y, w = w, fv = sigma,  :
+# The deviance has increased in an inner iteration for sigma
+# If persist, try different steps or model maybe inappropriate
+
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpond_gamlss, family=BE)
+# Error in RS() : The global deviance is increasing 
+# Try different steps for the parameters or the model maybe inappropriate
+
+########################## 
+# Beta regression        #
+# dataONEperpondoutliers #
+# link: logit            #
+# Variable dispersion    #
+##########################
+head(dataONEperpondoutliers)
+
+# re-format data so it has no NAs 
+dataONEperpondoutliers_gamlss <- cbind(dataONEperpondoutliers$FPcover_max,dataONEperpondoutliers$TOTP_avg)
+dataONEperpondoutliers_gamlss <- as.data.frame(dataONEperpondoutliers_gamlss) # convert to a data frame 
+dataONEperpondoutliers_gamlss <- dataONEperpondoutliers_gamlss[complete.cases(dataONEperpondoutliers_gamlss),]# remove any NAs
+names(dataONEperpondoutliers_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataONEperpondoutliers_gamlss)
+dataONEperpondoutliers_gamlss
+
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataONEperpondoutliers_gamlss, family=BE)
+
 
 ####################### 
 # Beta regression     #
@@ -73,10 +123,59 @@ dataFP_gamlss
 gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataFP_flexmix, family=BE)
 
 
+####################### 
+# Beta regression     #
+# dataFPoutliers      #
+# link: logit         #
+# Variable dispersion #
+#######################
+head(dataFPoutliers)
 
+# re-format data so it has no NAs 
+dataFPoutliers_gamlss <- cbind(dataFPoutliers$FPcover_max,dataFPoutliers$TOTP_avg)
+dataFPoutliers_gamlss <- as.data.frame(dataFPoutliers_gamlss) # convert to a data frame 
+dataFPoutliers_gamlss <- dataFPoutliers_gamlss[complete.cases(dataFPoutliers_gamlss),]# remove any NAs
+names(dataFPoutliers_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPoutliers_gamlss)
+dataFPoutliers_gamlss
 
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataFPoutliers_gamlss, family=BE)
 
+####################### 
+# Beta regression     #
+# dataFPsmall         #
+# link: logit         #
+# Variable dispersion #
+#######################
+head(dataFPsmall)
 
+# re-format data so it has no NAs 
+dataFPsmall_gamlss <- cbind(dataFPsmall$FPcover_max,dataFPsmall$TOTP_avg)
+dataFPsmall_gamlss <- as.data.frame(dataFPsmall_gamlss) # convert to a data frame 
+dataFPsmall_gamlss <- dataFPsmall_gamlss[complete.cases(dataFPsmall_gamlss),]# remove any NAs
+names(dataFPsmall_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPsmall_gamlss)
+dataFPsmall_gamlss
+
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataFPsmall_gamlss, family=BE)
+
+####################### 
+# Beta regression     #
+# dataFPoutlierssmall #
+# link: logit         #
+# Variable dispersion #
+#######################
+head(dataFPoutlierssmall)
+
+# re-format data so it has no NAs 
+dataFPoutlierssmall_gamlss <- cbind(dataFPoutlierssmall$FPcover_max,dataFPoutlierssmall$TOTP_avg)
+dataFPoutlierssmall_gamlss <- as.data.frame(dataFPoutlierssmall_gamlss) # convert to a data frame 
+dataFPoutlierssmall_gamlss <- dataFPoutlierssmall_gamlss[complete.cases(dataFPoutlierssmall_gamlss),]# remove any NAs
+names(dataFPoutlierssmall_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPoutlierssmall_gamlss)
+dataFPoutlierssmall_gamlss
+
+gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ TOTP_avg, data=dataFPoutlierssmall_gamlss, family=BE)
 
 
 
