8320889cd55c4ffbd238adc1cca0ae5954412dd6
mccannecology <mccannmikejames@gmail.com>
2014-04-13 10:41:32 -0400

    tried to get non-linear dispersion with gamlss

--- code/beta regression - quadratic dispersion - gamlss.R
+++ code/beta regression - quadratic dispersion - gamlss.R
@@ -69,49 +69,26 @@ names(dataONEperpond_gamlss) <- c("FPcover_max","TOTP_avg")
 head(dataONEperpond_gamlss)
 dataONEperpond_gamlss
 
+# I tried other additive tersm for gamlss - pb(), poly(), cs(), fp(), nl(), and vc() - but they did not work 
 
+betareg_dataONEperpond_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataONEperpond_gamlss, family=BE)
+summary(betareg_dataONEperpond_gamlss_logit_loessdisp)
+AIC(betareg_dataONEperpond_gamlss_logit_loessdisp)
+logLik(betareg_dataONEperpond_gamlss_logit_loessdisp)
 
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ vc(TOTP_avg), data=dataONEperpond_gamlss, family=BE)
-# Error in eval(expr, envir, enclos) : could not find function "vc"
+# extract sigma fitted values 
+fitted(betareg_dataONEperpond_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataONEperpond_gamlss_logit_loessdisp,what="sigma")
 
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ cs(TOTP_avg), data=dataONEperpond_gamlss, family=BE)
-# Error in RS() : The global deviance is increasing 
+# arrange the plots 
+par(mfrow=c(2,1))
 
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = fp(FPcover_maxx ~ TOTP_avg), data=dataONEperpond_gamlss, family=BE)
-# Error in array(x, c(length(x), 1L), if (!is.null(names(x))) list(names(x),  : invalid first argument
-                                                                   
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ fp(TOTP_avg), data=dataONEperpond_gamlss, family=BE)
-# Error in gamlss.fp(data[["fp(TOTP_avg)"]], z, w, 2) : object 'powerbest' not found
+# plot fitted model 
+plot(dataONEperpond$FPcover_max ~ dataONEperpond$TOTP_avg,main="dataONEperpond - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataONEperpond$TOTP_avg, dataONEperpond$TOTP_avg >0),fitted(betareg_dataONEperpond_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
 
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE(mu.link="identity",sigma.link="identity"))
-# Error in dBE(y, mu, sigma, log = TRUE) : mu must be between 0 and 1 
-
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = TOTP_avg ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE)
-#Error in RS() : The global deviance is increasing 
-# Try different steps for the parameters or the model maybe inappropriate
-# In addition: Warning message:
-# In glim.fit(f = sigma.object, X = sigma.X, y = y, w = w, fv = sigma,  :
-# The deviance has increased in an inner iteration for sigma
-# If persist, try different steps or model maybe inappropriate
-
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE(mu.link="log",sigma.link="log"))
-# Error in dBE(y, mu, sigma, log = TRUE) : mu must be between 0 and 1 
-
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE(mu.link="logit",sigma.link="logit"))
-# Error in RS() : The global deviance is increasing 
-# Try different steps for the parameters or the model maybe inappropriate
-# In addition: Warning message:
-# In glim.fit(f = sigma.object, X = sigma.X, y = y, w = w, fv = sigma,  :
-# The deviance has increased in an inner iteration for sigma
-# If persist, try different steps or model maybe inappropriate
-
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ poly(TOTP_avg,2), data=dataONEperpond_gamlss, family=BE)
-# Error in RS() : The global deviance is increasing 
-# Try different steps for the parameters or the model maybe inappropriate
-
-gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpond_gamlss, family=BE)
-# Error in RS() : The global deviance is increasing 
-# Try different steps for the parameters or the model maybe inappropriate
+# plot the variable dispersion
+plot(fitted(betareg_dataONEperpond_gamlss_logit_loessdisp,what="sigma") ~ subset(dataONEperpond$TOTP_avg, dataONEperpond$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
 
 ########################## 
 # Beta regression        #
@@ -120,6 +97,34 @@ gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpo
 # Variable dispersion    #
 # quadratic dispersion   #
 ##########################
+head(dataONEperpondoutliers)
+
+# re-format data so it has no NAs 
+dataONEperpondoutliers_gamlss <- cbind(dataONEperpondoutliers$FPcover_max,dataONEperpondoutliers$TOTP_avg)
+dataONEperpondoutliers_gamlss <- as.data.frame(dataONEperpondoutliers_gamlss) # convert to a data frame 
+dataONEperpondoutliers_gamlss <- dataONEperpondoutliers_gamlss[complete.cases(dataONEperpondoutliers_gamlss),]# remove any NAs
+names(dataONEperpondoutliers_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataONEperpondoutliers_gamlss)
+dataONEperpondoutliers_gamlss
+
+betareg_dataONEperpondoutliers_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataONEperpondoutliers_gamlss, family=BE)
+summary(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp)
+AIC(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp)
+logLik(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp)
+
+# extract sigma fitted values 
+fitted(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp,what="sigma")
+
+# arrange the plots 
+par(mfrow=c(2,1))
+
+# plot fitted model 
+plot(dataONEperpondoutliers$FPcover_max ~ dataONEperpondoutliers$TOTP_avg,main="dataONEperpondoutliers - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataONEperpondoutliers$TOTP_avg, dataONEperpondoutliers$TOTP_avg >0),fitted(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
+
+# plot the variable dispersion
+plot(fitted(betareg_dataONEperpondoutliers_gamlss_logit_loessdisp,what="sigma") ~ subset(dataONEperpondoutliers$TOTP_avg, dataONEperpondoutliers$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
 
 ########################## 
 # Beta regression        #
@@ -128,6 +133,34 @@ gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpo
 # Variable dispersion    #
 # quadratic dispersion   #
 ##########################
+head(dataFP)
+
+# re-format data so it has no NAs 
+dataFP_gamlss <- cbind(dataFP$FPcover_max,dataFP$TOTP_avg)
+dataFP_gamlss <- as.data.frame(dataFP_gamlss) # convert to a data frame 
+dataFP_gamlss <- dataFP_gamlss[complete.cases(dataFP_gamlss),]# remove any NAs
+names(dataFP_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFP_gamlss)
+dataFP_gamlss
+
+betareg_dataFP_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataFP_gamlss, family=BE)
+summary(betareg_dataFP_gamlss_logit_loessdisp)
+AIC(betareg_dataFP_gamlss_logit_loessdisp)
+logLik(betareg_dataFP_gamlss_logit_loessdisp)
+
+# extract sigma fitted values 
+fitted(betareg_dataFP_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataFP_gamlss_logit_loessdisp,what="sigma")
+
+# arrange the plots 
+par(mfrow=c(2,1))
+
+# plot fitted model 
+plot(dataFP$FPcover_max ~ dataFP$TOTP_avg,main="dataFP - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataFP$TOTP_avg, dataFP$TOTP_avg >0),fitted(betareg_dataFP_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
+
+# plot the variable dispersion
+plot(fitted(betareg_dataFP_gamlss_logit_loessdisp,what="sigma") ~ subset(dataFP$TOTP_avg, dataFP$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
 
 ########################## 
 # Beta regression        #
@@ -136,6 +169,34 @@ gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpo
 # Variable dispersion    #
 # quadratic dispersion   #
 ##########################
+head(dataFPoutliers)
+
+# re-format data so it has no NAs 
+dataFPoutliers_gamlss <- cbind(dataFPoutliers$FPcover_max,dataFPoutliers$TOTP_avg)
+dataFPoutliers_gamlss <- as.data.frame(dataFPoutliers_gamlss) # convert to a data frame 
+dataFPoutliers_gamlss <- dataFPoutliers_gamlss[complete.cases(dataFPoutliers_gamlss),]# remove any NAs
+names(dataFPoutliers_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPoutliers_gamlss)
+dataFPoutliers_gamlss
+
+betareg_dataFPoutliers_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataFPoutliers_gamlss, family=BE)
+summary(betareg_dataFPoutliers_gamlss_logit_loessdisp)
+AIC(betareg_dataFPoutliers_gamlss_logit_loessdisp)
+logLik(betareg_dataFPoutliers_gamlss_logit_loessdisp)
+
+# extract sigma fitted values 
+fitted(betareg_dataFPoutliers_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataFPoutliers_gamlss_logit_loessdisp,what="sigma")
+
+# arrange the plots 
+par(mfrow=c(2,1))
+
+# plot fitted model 
+plot(dataFPoutliers$FPcover_max ~ dataFPoutliers$TOTP_avg,main="dataFPoutliers - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataFPoutliers$TOTP_avg, dataFPoutliers$TOTP_avg >0),fitted(betareg_dataFPoutliers_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
+
+# plot the variable dispersion
+plot(fitted(betareg_dataFPoutliers_gamlss_logit_loessdisp,what="sigma") ~ subset(dataFPoutliers$TOTP_avg, dataFPoutliers$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
 
 ########################## 
 # Beta regression        #
@@ -144,11 +205,67 @@ gamlss(FPcover_max ~ TOTP_avg, sigma.formula = ~ pb(TOTP_avg), data=dataONEperpo
 # Variable dispersion    #
 # quadratic dispersion   #
 ##########################
+head(dataFPsmall)
+
+# re-format data so it has no NAs 
+dataFPsmall_gamlss <- cbind(dataFPsmall$FPcover_max,dataFPsmall$TOTP_avg)
+dataFPsmall_gamlss <- as.data.frame(dataFPsmall_gamlss) # convert to a data frame 
+dataFPsmall_gamlss <- dataFPsmall_gamlss[complete.cases(dataFPsmall_gamlss),]# remove any NAs
+names(dataFPsmall_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPsmall_gamlss)
+dataFPsmall_gamlss
+
+betareg_dataFPsmall_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataFPsmall_gamlss, family=BE)
+summary(betareg_dataFPsmall_gamlss_logit_loessdisp)
+AIC(betareg_dataFPsmall_gamlss_logit_loessdisp)
+logLik(betareg_dataFPsmall_gamlss_logit_loessdisp)
+
+# extract sigma fitted values 
+fitted(betareg_dataFPsmall_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataFPsmall_gamlss_logit_loessdisp,what="sigma")
+
+# arrange the plots 
+par(mfrow=c(2,1))
+
+# plot fitted model 
+plot(dataFPsmall$FPcover_max ~ dataFPsmall$TOTP_avg,main="dataFPsmall - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataFPsmall$TOTP_avg, dataFPsmall$TOTP_avg >0),fitted(betareg_dataFPsmall_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
+
+# plot the variable dispersion
+plot(fitted(betareg_dataFPsmall_gamlss_logit_loessdisp,what="sigma") ~ subset(dataFPsmall$TOTP_avg, dataFPsmall$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
 
 ########################## 
 # Beta regression        #
-# dataFPsmalloutliers    #
+# dataFPoutlierssmall    #
 # link: logit            #
 # Variable dispersion    #
 # quadratic dispersion   #
 ##########################
+head(dataFPoutlierssmall)
+
+# re-format data so it has no NAs 
+dataFPoutlierssmall_gamlss <- cbind(dataFPoutlierssmall$FPcover_max,dataFPoutlierssmall$TOTP_avg)
+dataFPoutlierssmall_gamlss <- as.data.frame(dataFPoutlierssmall_gamlss) # convert to a data frame 
+dataFPoutlierssmall_gamlss <- dataFPoutlierssmall_gamlss[complete.cases(dataFPoutlierssmall_gamlss),]# remove any NAs
+names(dataFPoutlierssmall_gamlss) <- c("FPcover_max","TOTP_avg")
+head(dataFPoutlierssmall_gamlss)
+dataFPoutlierssmall_gamlss
+
+betareg_dataFPoutlierssmall_gamlss_logit_loessdisp <- gamlss(FPcover_max ~ TOTP_avg, sigma.formula = lo( ~TOTP_avg), data=dataFPoutlierssmall_gamlss, family=BE)
+summary(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp)
+AIC(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp)
+logLik(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp)
+
+# extract sigma fitted values 
+fitted(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp,what="mu")
+fitted(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp,what="sigma")
+
+# arrange the plots 
+par(mfrow=c(2,1))
+
+# plot fitted model 
+plot(dataFPoutlierssmall$FPcover_max ~ dataFPoutlierssmall$TOTP_avg,main="dataFPoutlierssmall - gamlss - logit link",xlab="Total P (mg/L)",ylab="FP cover",log="x")
+lines(subset(dataFPoutlierssmall$TOTP_avg, dataFPoutlierssmall$TOTP_avg >0),fitted(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp,what="mu"),type="p",col="red")
+
+# plot the variable dispersion
+plot(fitted(betareg_dataFPoutlierssmall_gamlss_logit_loessdisp,what="sigma") ~ subset(dataFPoutlierssmall$TOTP_avg, dataFPoutlierssmall$TOTP_avg >0),xlab="Total P (mg/L)",ylab="Sigma (scale parameter)",log="x")
