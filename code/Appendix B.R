e37c8f73e50e197600950a664b5bafbb1db37aa9
mccannecology <mccannmikejames@gmail.com>
2014-04-15 10:28:18 -0400

    updating appendices for new data

--- code/Appendix B.R
+++ code/Appendix B.R
@@ -6,11 +6,8 @@
 
 # convert FP_species_richness to a factor 
 dataONEperpond$FP_species_richness <- as.factor(dataONEperpond$FP_species_richness)
-
 appendixB <- ggplot(data=dataONEperpond, aes(x=FP_species_richness)) + geom_bar(width=0.9, position = position_dodge(width=1)) # plot 
-
 appendixB <- appendixB + xlab("Floating Plant Species Richness") + ylab("Frequency")
-
+appendixB <- appendixB + theme_classic(base_size=18)
 appendixB
-
-ggsave(file="histogram - FP species richness - all ponds.jpg")
+ggsave(file="histogram - FP species richness - all ponds.jpg", appendixB, height=5,width=5)
--- code/Appendix C.R
+++ code/Appendix C.R
@@ -8,15 +8,10 @@
 dataFP$FPcover_max_percent <- dataFP$FPcover_max*100 
 
 appendixC <-ggplot(data=dataFP, aes(x=surfacearea_ha,y=FPcover_max_percent)) + geom_point(size=3) 
-
 appendixC <- appendixC + scale_x_log10()
-
 appendixC <- appendixC + geom_vline(xintercept = 5,colour="red",size=1,linetype="longdash") 
-
 appendixC <- appendixC + ylab("Floating plant cover (%)") + xlab("Surface area (ha)")
-
 appendixC <- appendixC + theme_classic(base_size=18)
-
 appendixC
 
-ggsave(file="scatterplot - log surface area ~ FP cover max - FP ponds - line at 5ha.jpg")
+ggsave(file="scatterplot - log surface area ~ FP cover max - FP ponds - line at 5ha.jpg", appendixC, height=5,width=5)
