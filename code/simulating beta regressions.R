e20c37448fb50c78affe4ee7b7da64dd2f6d9c20
mccannecology <mccannmikejames@gmail.com>
2014-04-29 14:26:03 -0400

    added some more simulated circumstances

--- code/simulating beta regressions.R
+++ code/simulating beta regressions.R
@@ -41,20 +41,31 @@ temp <- cbind(dataFP$TOTP_avg, rbeta(n,a,b))
 plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
 
 #################################
+# no slope 
+# Just an intercept @ 0.8 
+# very hight percision 
+#################################
+n=length(dataFP$FPcover_max)
+mu = 0.8 
+phi = 100
+a = mu*phi
+b = (-mu*phi) + phi
+temp <- cbind(dataFP$TOTP_avg, rbeta(n,a,b))
+plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
+
+#################################
 # with a slope 
 # very high percision 
 # this might not be the best slope
 # it should probably hit a ceiling before TOTP ~ 0.5 
 #################################
 # this is the slope of the line 
-m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / (max(dataFP$TOTP_avg,na.rm=T) - min(dataFP$TOTP_avg,na.rm=T))
-# this is the intercept of the line 
-intercept = min(dataFP$FPcover_max,na.rm=T)
+m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / 
+  (max(dataFP$TOTP_avg,na.rm=T) - min(dataFP$TOTP_avg,na.rm=T))
+intercept = min(dataFP$FPcover_max,na.rm=T) # this is the intercept of the line 
 
 phi = 100
-
-# make a blank vector of mu
-mu <- rep(0,length(dataFP$TOTP_avg))
+mu <- rep(0,length(dataFP$TOTP_avg)) # make a blank vector of mu
 
 # fill it will approprate mu values based on the regression on TOTP 
 for (i in 1:length(mu)) {
@@ -87,9 +98,9 @@ plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
 # slope hits a ceiling at TOTP ~ 0.5 
 #################################
 # this is the slope of the line 
-m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / (0.5 - min(dataFP$TOTP_avg,na.rm=T))
-# this is the intercept of the line 
-intercept = min(dataFP$FPcover_max,na.rm=T)
+m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / 
+  (0.5 - min(dataFP$TOTP_avg,na.rm=T))
+intercept = min(dataFP$FPcover_max,na.rm=T) # this is the intercept of the line 
 
 # make a blank vector of mu
 mu <- rep(0,length(dataFP$TOTP_avg))
@@ -123,6 +134,89 @@ for (i in 1:length(a)) {
 # plot it  
 plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
 
+#################################
+# with a slope 
+# very high percision 
+# slope hits a ceiling at TOTP ~ 0.1 
+#################################
+# this is the slope of the line 
+m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / 
+  (0.1 - min(dataFP$TOTP_avg,na.rm=T))
+intercept = min(dataFP$FPcover_max,na.rm=T) # this is the intercept of the line 
+
+# make a blank vector of mu
+mu <- rep(0,length(dataFP$TOTP_avg))
+phi <- 100
+
+# fill it will approprate mu values based on the regression on TOTP 
+for (i in 1:length(mu)) {
+  if (is.na(dataFP$TOTP_avg[i]) == T | dataFP$TOTP_avg[i] < 0.5) {
+    mu[i] <- m*dataFP$TOTP_avg[i] + intercept
+  }
+  else (mu[i] <- max(dataFP$FPcover_max,na.rm=T))
+}
+
+# make a blank vector of a and b
+a <- rep(0,length(dataFP$TOTP_avg))
+b <- rep(0,length(dataFP$TOTP_avg))
+
+# figure out the a and b for each mu 
+for (i in 1:length(a)) {
+  a[i] <- mu[i] * phi 
+  b[i] <- (-mu[i]*phi) + phi
+}
+
+# generate the random betas with each mu valuae 
+temp <- matrix(rep(0,2*length(dataFP$TOTP_avg)),ncol=2)
+for (i in 1:length(a)) {
+  temp[i,1] <- dataFP$TOTP_avg[i]
+  temp[i,2] <- rbeta(1,a[i],b[i])
+}
+
+# plot it  
+plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
+
+#################################
+# with a slope 
+# very moderate percision - 30
+# slope hits a ceiling at TOTP ~ 0.1 
+#################################
+# this is the slope of the line 
+m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / 
+  (0.1 - min(dataFP$TOTP_avg,na.rm=T))
+intercept = min(dataFP$FPcover_max,na.rm=T) # this is the intercept of the line 
+
+# make a blank vector of mu
+mu <- rep(0,length(dataFP$TOTP_avg))
+phi <- 30
+
+# fill it will approprate mu values based on the regression on TOTP 
+for (i in 1:length(mu)) {
+  if (is.na(dataFP$TOTP_avg[i]) == T | dataFP$TOTP_avg[i] < 0.5) {
+    mu[i] <- m*dataFP$TOTP_avg[i] + intercept
+  }
+  else (mu[i] <- max(dataFP$FPcover_max,na.rm=T))
+}
+
+# make a blank vector of a and b
+a <- rep(0,length(dataFP$TOTP_avg))
+b <- rep(0,length(dataFP$TOTP_avg))
+
+# figure out the a and b for each mu 
+for (i in 1:length(a)) {
+  a[i] <- mu[i] * phi 
+  b[i] <- (-mu[i]*phi) + phi
+}
+
+# generate the random betas with each mu valuae 
+temp <- matrix(rep(0,2*length(dataFP$TOTP_avg)),ncol=2)
+for (i in 1:length(a)) {
+  temp[i,1] <- dataFP$TOTP_avg[i]
+  temp[i,2] <- rbeta(1,a[i],b[i])
+}
+
+# plot it  
+plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
 
 #################################
 # with a slope 
@@ -131,6 +225,48 @@ plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
 #################################
 # this is the slope of the line 
 m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / (0.5 - min(dataFP$TOTP_avg,na.rm=T))
+# this is the intercept of the line 
+intercept = min(dataFP$FPcover_max,na.rm=T)
+
+# make a blank vector of mu
+mu <- rep(0,length(dataFP$TOTP_avg))
+phi <- 1
+
+# fill it will approprate mu values based on the regression on TOTP 
+for (i in 1:length(mu)) {
+  if (is.na(dataFP$TOTP_avg[i]) == T | dataFP$TOTP_avg[i] < 0.5) {
+    mu[i] <- m*dataFP$TOTP_avg[i] + intercept
+  }
+  else (mu[i] <- max(dataFP$FPcover_max,na.rm=T))
+}
+
+# make a blank vector of a and b
+a <- rep(0,length(dataFP$TOTP_avg))
+b <- rep(0,length(dataFP$TOTP_avg))
+
+# figure out the a and b for each mu 
+for (i in 1:length(a)) {
+  a[i] <- mu[i] * phi 
+  b[i] <- (-mu[i]*phi) + phi
+}
+
+# generate the random betas with each mu valuae 
+temp <- matrix(rep(0,2*length(dataFP$TOTP_avg)),ncol=2)
+for (i in 1:length(a)) {
+  temp[i,1] <- dataFP$TOTP_avg[i]
+  temp[i,2] <- rbeta(1,a[i],b[i])
+}
+
+# plot it  
+plot(temp[,1],temp[,2],log="x",ylim=c(0,1))
+
+################################
+# with a slope 
+# very low percision 
+# slope hits a ceiling at TOTP ~ 0.1 
+#################################
+# this is the slope of the line 
+m = (max(dataFP$FPcover_max,na.rm=T) - min(dataFP$FPcover_max,na.rm=T)) / (0.1 - min(dataFP$TOTP_avg,na.rm=T))
 # this is the intercept of the line 
 intercept = min(dataFP$FPcover_max,na.rm=T)
 
